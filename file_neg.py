6279ac03bf9976dd8e932697704aef4e5e96bed7
Д Ням-Очир <nyamochirshde@gmail.com>
2024-06-11 17:32:49 +0800

    branch neg iin file_neg.py iig oorchilson

--- file_neg.py
+++ file_neg.py
@@ -1,1 +1,16 @@
-print ("Hello, World!")
+test_list = [12, 67, 98, 34, 111]
+
+# printing original list
+print("The original list is : " + str(test_list))
+
+# Sum of number digits in List
+# using loop + str()
+res = []
+for ele in test_list:
+    sum = 0
+    for digit in str(ele):
+        sum += int(digit)
+    res.append(sum)
+    
+# printing result 
+print ("List Integer Summation : " + str(res))
